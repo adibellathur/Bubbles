2c70eff7280c8e26165ecd85739d63709fc3cf63
Adithya Bellathur <bellathu@usc.edu>
2018-11-03 11:10:51 -0700

    Added gitignore

--- bubble-app-simple/screens/Library.js
+++ bubble-app-simple/screens/Library.js
@@ -2,18 +2,6 @@ import React, {Component} from 'react';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 class Library extends Component {
-    // return (
-    //     <SafeAreaView style={{flex:1}}>
-    //         <View style={{flex:1}}>
-    //             <View style={{height:80, backgroundColor: 'white',
-    //             bottomBorderWidth: 1, bottomBorderColor: '#dddddd'}}>
-    //                 <Text>
-    //                     Library
-    //                 </Text>
-    //             </View>
-    //         </View>
-    //     </SafeAreaView>
-    // );
     render() {
       return (
         <View styles={{
