20652930db2d00ffd389e9410d05993f32453d7f
Adithya Bellathur <bellathu@usc.edu>
2018-11-03 13:10:53 -0700

    Adding stuff to library file

--- bubble-app-simple/screens/Library.js
+++ bubble-app-simple/screens/Library.js
@@ -1,12 +1,22 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, StatusBar } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 class Library extends Component {
     render() {
       return (
-        <View style={styles.container}>
-          <Text>Library</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+          <View>
+            <StatusBar backgroundColor="blue" barStyle="light-content"/>
+          </View>
+          <View style={styles.header}>
+            <Text style={styles.title}>Library</Text>
+          </View>
+          <View style={styles.searchbar}>
+            <Icon name='ios-search' size={30} />
+          </View>
+        </SafeAreaView>
+
       );
     }
 }
@@ -15,8 +25,24 @@ export default Library;
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
+        flex: 1
+    },
+    header: {
+      height: 100,
+      backgroundColor: 'white',
+      borderBottomWidth: 1,
+      borderBottomColor: '#dddddd',
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    title: {
+      paddingTop: 20,
+      color: 'blue',
+      fontSize: 20
+    },
+    searchbar: {
+      paddingLeft: 50,
+      height: 50,
+      justifyContent: 'center'
     }
 });
